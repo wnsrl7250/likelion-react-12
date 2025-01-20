b53d5d874960745d724f7580720f9f1c2a012fcf
wnsrl7250 <wnsrl7250@naver.com>
2025-01-20 16:35:08 +0900

    [실습] 백엔드 프로그램 II
    - 클라이언트 앱 ~ 서버 앱 요청/응답 처리

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,20 @@ import express from 'express';
 const app = express();
 
 // 라우팅(Routing)
-app.get('/api/user', (request, response) => {
-  response.send('hello express.js app');
+app.get('/api/hello', (request, response) => {
+  const { username, useremail } = request.query;
+  if (username && useremail) {
+    response.status(200).send(`
+      <h1>hello ${username}!</h1>
+      <p>your email address is ${useremail}</p>
+    `);
+  } else {
+    response
+      .status(400)
+      .send('<p>사용자 이름과 이메일이 전송되지 않았습니다. 😥</p>');
+  }
 });
 
 app.listen(4000, () => {
-  console.log('백엔드 프로그램 서버 구동 http://localhost:4000/api/user');
+  console.log('백엔드 프로그램 서버 구동 http://localhost:4000/api/hello');
 });
