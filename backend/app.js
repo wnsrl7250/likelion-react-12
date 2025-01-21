5e466e4aa01a2cceedc3c7f70b483807d6e74564
wnsrl7250 <wnsrl7250@naver.com>
2025-01-21 11:36:52 -0500

    [실습] 회원가입 처리 IV
    - 회원가입 기능 구현

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,5 @@
-import bcrypt from 'bcrypt';
 import express from 'express';
 import { createUser } from './lib/user.js';
-
-console.log(bcrypt);
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
--- backend/lib/user.js
+++ backend/lib/user.js
@@ -1,6 +1,8 @@
-import { readFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
 import { fileURLToPath } from 'node:url';
+import bcrypt from 'bcrypt';
 
+const saltRounds = 9;
 const FILE_PATH = fileURLToPath(new URL('../data/users.json', import.meta.url));
 const OPTIONS = { encoding: 'utf-8' };
 
@@ -14,9 +16,37 @@ export async function findUserByEmail(email) {
   return users.find((user) => user.email === email);
 }
 
-const user = await findUserByEmail('hanj2@kakao.com');
-console.log(user);
+export async function createUser(newUser) {
+  // 이미 가입한 사용자인가요?
+  const user = await findUserByEmail(newUser.email);
+  // 이미 가입한 사용자라면 null 반환하고, 함수 종료합시다.
+  // 서버에서 클라이언트에 이미 회원가입한 사용자 임을 응답합니다.
+  if (user) return null;
+
+  // 새 가입자라면 사용자 정보를 data/users.json에 저장합니다.
+  // [중요] 요청받은 사용자의 민감한 정보인 패스워드는 암호화합니다.
+
+  const users = await getUsers();
+  const hashedPassword = await bcrypt.hash(newUser.password, saltRounds);
+
+  const createdUser = {
+    id: crypto.randomUUID(),
+    name: newUser.name,
+    email: newUser.email,
+    password: hashedPassword,
+  };
+
+  users.push(createdUser);
+
+  await writeFile(FILE_PATH, JSON.stringify(users, null, 2), OPTIONS);
+
+  return createdUser;
+}
+
+createUser({
+  name: '박현주',
+  email: 'park@naver.com',
+  password: 'qwerty!@',
+});
 
 export async function isRegisteredUser() {}
-
-export async function createUser() {}
