976e0d1be2fd32a1e14c5e62bb24438a4b8367b3
wnsrl7250 <wnsrl7250@naver.com>
2025-01-21 16:20:22 +0900

    [실습] 컴포넌트 설계
    - 엄격(Strict)
    - 느슨(Loose) : restProps

--- src/playground.tsx
+++ src/playground.tsx
@@ -1,7 +1,10 @@
+import ReactForm from './playgrounds/02-react-form';
+
 function Playground() {
   return (
     <div className="Playground">
       <h1>플레이그라운드</h1>
+      <ReactForm />
     </div>
   );
 }
