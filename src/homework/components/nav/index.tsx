3447f340fd60f0047b693ea845d803c343daf045
wnsrl7250 <wnsrl7250@naver.com>
2025-02-04 09:23:56 +0900

    [실습] 내비게이션 리스트 데이터 렌더링
    - 내비게이션 리스트 데이터 작성
    - 리스트 렌더링

--- src/homework/components/nav/index.tsx
+++ src/homework/components/nav/index.tsx
@@ -12,18 +12,35 @@ function Nav() {
   const isSignUpView = uiView.includes('signup');
   const isStateManagementView = uiView.includes('state-management');
 
+  // 내비게이션 리스트
+  const navList = [
+    { id: 'item-1', content: '로그인', href: 'signin', isActive: isSignInView },
+    {
+      id: 'item-2',
+      content: '회원가입',
+      href: 'signup',
+      isActive: isSignUpView,
+    },
+    {
+      id: 'item-3',
+      content: '상태 관리 전략',
+      href: 'state-management',
+      isActive: isStateManagementView,
+    },
+  ];
+
   return (
     <nav className="nav">
       <h2 className="sr-only">페이지 탐색</h2>
-      <NavLink href="signin" isActive={isSignInView}>
-        로그인
-      </NavLink>
-      <NavLink href="signup" isActive={isSignUpView}>
-        회원가입
-      </NavLink>
-      <NavLink href="state-management" isActive={isStateManagementView}>
-        상태 관리 전략
-      </NavLink>
+      {navList.map((navItem) => (
+        <NavLink
+          key={navItem.id}
+          href={navItem.href}
+          isActive={navItem.isActive}
+        >
+          {navItem.content}
+        </NavLink>
+      ))}
     </nav>
   );
 }
