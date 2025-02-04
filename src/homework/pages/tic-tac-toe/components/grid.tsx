3950c6a81589d313b49bdbb0832c8e35b4277457
wnsrl7250 <wnsrl7250@naver.com>
2025-02-04 16:10:17 +0900

    [실습] 틱택토 게임 구현 part 2-1.
    - 게임 진행을 위한 handlePlay 함수 작성
    - 게임 상태 업데이트

--- src/homework/pages/tic-tac-toe/components/grid.tsx
+++ src/homework/pages/tic-tac-toe/components/grid.tsx
@@ -6,7 +6,7 @@ import Cell from './cell';
 function Grid() {
   // [상태]
   // 게임 보드 셀(cells, 9개(3 x 3))
-  const [cells] = useState<Cells>(INITIAL_CELLS);
+  const [cells, setCells] = useState<Cells>(INITIAL_CELLS);
   // 게임 순서(order)
   const [order, setOrder] = useState<number>(0);
 
@@ -15,19 +15,22 @@ function Grid() {
   const nextPlayer = order % 2 === 0 ? PLAYER.ONE : PLAYER.TWO;
 
   // [이벤트 핸들러]
-  // 게임 상태 업데이트
   const handlePlay = (index: number) => {
+    // 게임 상태 업데이트 (순서)
     const nextOrder = order + 1;
     setOrder(nextOrder);
-    console.log(index, 'click', nextPlayer);
+
+    // 게임 상태 업데이트 (게임 보드 셀)
+    const nextCells = cells.map((cell, i) => (index !== i ? cell : nextPlayer));
+    setCells(nextCells);
   };
 
   return (
     <div className={tm('grid grid-rows-3 grid-cols-3 gap-1')}>
-      {cells.map((_cell, index) => {
+      {cells.map((cell, index) => {
         return (
           <Cell key={index} onPlay={() => handlePlay(index)}>
-            {index}
+            {cell}
           </Cell>
         );
       })}
