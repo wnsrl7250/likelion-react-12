a9f74d313645281477cb0a7a328bce4b25f26a06
wnsrl7250 <wnsrl7250@naver.com>
2025-02-06 13:41:45 +0900

    [실습] 게임 운영 마무리 파트 진행
    - 게임 상태 메시지 표시
    - 게임 초기화 함수 설정

--- src/homework/pages/tic-tac-toe/components/board.tsx
+++ src/homework/pages/tic-tac-toe/components/board.tsx
@@ -52,10 +52,18 @@ function Board() {
     setCells(nextCells);
   };
 
+  // [이벤트 핸들러]
+  // 게임 초기화 함수
+  const handleReGame = () => {
+    // 게임 운영에 사용되는 상태 초기화 (리셋, 리-게임)
+    setCells(INITIAL_CELLS);
+    setOrder(0);
+  };
+
   return (
     <section className={tm('flex flex-col space-y-2 items-center', 'w-60')}>
       <h3 className="sr-only">게임 보드</h3>
-      <Status message={statusMessage} />
+      <Status message={statusMessage} onReGame={handleReGame} />
       <Grid cells={cells} winner={winner} onPlay={handlePlay} />
     </section>
   );
--- src/homework/pages/tic-tac-toe/components/status.tsx
+++ src/homework/pages/tic-tac-toe/components/status.tsx
@@ -1,9 +1,37 @@
+import { tm } from '@/utils/tw-merge';
+
 interface StatusProps {
   message: string;
+  onReGame?: () => void;
 }
 
-function Status({ message }: StatusProps) {
-  return <div>{message}</div>;
+function Status({ message, onReGame }: StatusProps) {
+  // [파생된 상태]
+  const isComplete = !message.includes('넥스트 플레이어');
+
+  const handleReGame = () => {
+    onReGame?.();
+  };
+
+  return (
+    <div className={tm('flex justify-between w-full px-5')}>
+      <p>{message}</p>
+      {isComplete && (
+        <button
+          type="button"
+          className={tm(
+            'cursor-pointer',
+            'px-1.5 py-1 border-1 border-slate-900 rounded-sm',
+            'text-xs font-semibold',
+            'hover:bg-slate-800/10'
+          )}
+          onClick={handleReGame}
+        >
+          한 게임 더?!
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default Status;
