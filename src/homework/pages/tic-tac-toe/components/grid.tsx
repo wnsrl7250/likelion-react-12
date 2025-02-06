19537d000e42a1a74ef2a9681a4eb5cdba0c6998
wnsrl7250 <wnsrl7250@naver.com>
2025-02-06 11:34:49 +0900

    [실습] 게임 위너
    - 게임 진행 중 위너 찾기 함수 작성
    - 위너가 있을 경우, 말판에 위너 스타일링

--- src/homework/pages/tic-tac-toe/components/grid.tsx
+++ src/homework/pages/tic-tac-toe/components/grid.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { tm } from '@/utils/tw-merge';
-import { INITIAL_CELLS, PLAYER, type Cells } from '../constants';
+import { getWinner, INITIAL_CELLS, PLAYER, type Cells } from '../constants';
 import Cell from './cell';
 
 function Grid() {
@@ -14,8 +14,21 @@ function Grid() {
   // 다음 플레이어(Next Player)는?
   const nextPlayer = order % 2 === 0 ? PLAYER.ONE : PLAYER.TWO;
 
+  // 현재 게임 턴에 승리자(Winner)가 있나요?
+  const winner = getWinner(cells);
+
   // [이벤트 핸들러]
+  // 게임 진행 함수
   const handlePlay = (index: number) => {
+    // 승리자가 있다면 게임 오버(GAME OVER)
+    if (winner) {
+      // 알림(notification)
+      alert(`GAME OVER!\nWinner ${winner.player}`);
+      return;
+    }
+
+    // 승리자가 없다면 게임 진행 계속
+
     // 게임 상태 업데이트 (순서)
     const nextOrder = order + 1;
     setOrder(nextOrder);
@@ -28,8 +41,23 @@ function Grid() {
   return (
     <div className={tm('grid grid-rows-3 grid-cols-3 gap-1')}>
       {cells.map((cell, index) => {
+        let winnerClasses = '';
+
+        // 승리자가 있다면?
+        if (winner) {
+          // 승리 패턴(공식)에 해당하는 말판에 색칠 공부하기!!
+          const [x, y, z] = winner.condition;
+          if (index === x || index === y || index === z) {
+            winnerClasses = 'outline-2 outline-amber-500 bg-amber-500/30';
+          }
+        }
+
         return (
-          <Cell key={index} onPlay={() => handlePlay(index)}>
+          <Cell
+            key={index}
+            className={winnerClasses}
+            onPlay={() => handlePlay(index)}
+          >
             {cell}
           </Cell>
         );
--- src/homework/pages/tic-tac-toe/constants.ts
+++ src/homework/pages/tic-tac-toe/constants.ts
@@ -1,5 +1,5 @@
 // ----------------------------------------------
-// 게임 상수
+// 게임 진행을 위한 상수 및 함수
 // ----------------------------------------------
 
 /* const INITIAL_CELLS = [null, null, null, null, null, null, null, null, null]; */
@@ -11,3 +11,40 @@ export const enum PLAYER {
 }
 
 export type Cells = (PLAYER | null)[];
+
+// 게임 승리 조건
+const WINNER_CONDITIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+// 게임 진행 검사 -> 게임 위너 반환 함수
+type Winner = {
+  player: PLAYER;
+  condition: [number, number, number];
+} | null;
+
+export const getWinner = (cells: Cells) => {
+  let winner = null;
+
+  // 승리 조건 순환
+  for (const [x, y, z] of WINNER_CONDITIONS) {
+    const player = cells[x];
+    // 승리 조건에 해당하는 위치에 말판이 일치하는 지 검사
+    if (player && cells[y] === player && cells[z] === player) {
+      winner = {
+        player,
+        condition: [x, y, z],
+      };
+      break;
+    }
+  }
+
+  return winner as Winner;
+};
