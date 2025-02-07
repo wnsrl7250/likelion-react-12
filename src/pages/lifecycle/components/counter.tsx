8c8cd369c31d44f1bccb0732af93909c114e82d9
wnsrl7250 <wnsrl7250@naver.com>
2025-02-07 11:50:26 +0900

    [실습] 클래스 컴포넌트 라이프 사이클 메서드 4
    - getSnapshotBeforeUpdate

--- src/pages/lifecycle/components/counter.tsx
+++ src/pages/lifecycle/components/counter.tsx
@@ -122,6 +122,17 @@ class Counter extends Component<Props, State> {
     );
   }
 
+  // [라이프사이클 메서드] ---------------------------------------------
+  getSnapshotBeforeUpdate() {
+    // 이전 속성 또는 상태와 현재 렌더링 시점의 속성 또는 상태 비교
+    // prevProps: Readonly<Props>,
+    // prevState: Readonly<State>
+    //
+    // ui가 부자연 스러우면... 스냅샷 데이터 반환
+    // snapshot 데이터
+    // return snapshot;
+  }
+
   // <클래스 필드>
   clearIntervalId: NodeJS.Timeout | number = 0;
 
@@ -146,6 +157,7 @@ class Counter extends Component<Props, State> {
   componentDidUpdate(
     _prevProps: Readonly<Props>,
     prevState: Readonly<State>
+    // _snapshot: unknown
   ): void {
     console.group('이전 상태 값');
     // console.log({ prevProps });
@@ -155,6 +167,9 @@ class Counter extends Component<Props, State> {
     console.group('현재 상태 값');
     console.log(this.state.count);
     console.groupEnd();
+
+    // 부자연스러운 UI 움직임을 정상적으로 보이도록 처리 (사이드 이펙트)
+    // 스냅샷 정보 활용
 
     // 사이드 이펙트
     // 리액트 렌더링 프로세스와 상관없는 일 처리
