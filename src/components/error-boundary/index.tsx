4e9d4218f6b858386315b365af4f488fe6a998b9
wnsrl7250 <wnsrl7250@naver.com>
2025-02-07 14:10:29 +0900

    [실습] 오류 경계 2
    - static getDerivedStateFromError 라이프 사이클 메서드 활용
    - 오류 감지 후, 대체 UI 화면에 표시

--- src/components/error-boundary/index.tsx
+++ src/components/error-boundary/index.tsx
@@ -1,24 +1,49 @@
 import { Component, type ErrorInfo } from 'react';
 
+interface Props {
+  children: React.ReactNode;
+}
+
 interface State {
   error: null | Error;
   errorInfo: null | ErrorInfo;
   hasError: boolean;
 }
 
-class ErrorBoundary extends Component {
+class ErrorBoundary extends Component<Props, State> {
   state: State = {
     error: null,
     errorInfo: null,
     hasError: false,
   };
 
+  // 오류 감지하기 위한 라이프 사이클 메서드
+  static getDerivedStateFromError(error: Error) {
+    // 상태 업데이트
+    return {
+      hasError: true,
+      error,
+    };
+  }
+  // componentDidCatch(error, errorInfo)
+
   render() {
-    return (
-      <div role="alert" lang="en">
-        <h2>Error Boundary</h2>
-      </div>
-    );
+    // 오류가 있으면
+    // 대체(fallback) 컴포넌트를 렌더링
+    if (this.state.hasError) {
+      const { error } = this.state;
+
+      return (
+        <div role="alert">
+          <h2>{error?.name} 오류 발생!</h2>
+          <p>{error?.message}</p>
+        </div>
+      );
+    }
+
+    // 오류가 없으면
+    // 하위 컴포넌트 렌더링
+    return this.props.children;
   }
 }
 
--- src/pages/error-boundary/wrapper.tsx
+++ src/pages/error-boundary/wrapper.tsx
@@ -3,7 +3,7 @@ import CounterClass from '../lifecycle/components/counter';
 
 function Wrapper() {
   return (
-    <div className="Wrapper p-5 border-3 border-black/20">
+    <div className="Wrapper my-5 p-5 border-3 border-black/20">
       <h3 className="text-lg font-semibold">래퍼 컴포넌트</h3>
       <Counter min={4} max={12} count={6} />
       <hr />
