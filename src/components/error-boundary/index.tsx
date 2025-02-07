17377b6723fce2c3274183f70184028161b0c640
wnsrl7250 <wnsrl7250@naver.com>
2025-02-07 14:18:15 +0900

    [실습] 오류 경계 3
    - componentDidCatch 라이프 사이클 메서드 활용
    - 오류 발생 시, 오류가 발생한 컴포넌트 스택 정보 화면에 표시

--- src/components/error-boundary/index.tsx
+++ src/components/error-boundary/index.tsx
@@ -1,3 +1,4 @@
+import { tm } from '@/utils/tw-merge';
 import { Component, type ErrorInfo } from 'react';
 
 interface Props {
@@ -18,25 +19,46 @@ class ErrorBoundary extends Component<Props, State> {
   };
 
   // 오류 감지하기 위한 라이프 사이클 메서드
-  static getDerivedStateFromError(error: Error) {
-    // 상태 업데이트
-    return {
+  // static getDerivedStateFromError(error: Error) {
+  //   // 상태 업데이트
+  //   return {
+  //     hasError: true,
+  //     error,
+  //   };
+  // }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    this.setState({
       hasError: true,
+      errorInfo,
       error,
-    };
+    });
   }
-  // componentDidCatch(error, errorInfo)
 
   render() {
     // 오류가 있으면
     // 대체(fallback) 컴포넌트를 렌더링
     if (this.state.hasError) {
-      const { error } = this.state;
+      const { error, errorInfo } = this.state;
 
       return (
-        <div role="alert">
-          <h2>{error?.name} 오류 발생!</h2>
-          <p>{error?.message}</p>
+        <div
+          role="alert"
+          className={tm(
+            'overflow-x-scroll',
+            'flex flex-col gap-2',
+            'border-4 border-red-600',
+            'p-5',
+            ' text-red-600'
+          )}
+        >
+          <h2 className={tm('text-xl font-semibold')}>
+            {error?.name} 오류 발생!
+          </h2>
+          <p className={tm('text-red-700 font-normal')}>{error?.message}</p>
+          <pre className="-ml-8 text-xs leading-[2]">
+            {errorInfo?.componentStack}
+          </pre>
         </div>
       );
     }
