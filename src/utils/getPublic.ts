7da946c6a7d9c1bc1779b4805b2d8da7b547da90
wnsrl7250 <wnsrl7250@naver.com>
2025-02-10 10:33:27 +0900

    [추가] getPublicIcon 유틸리티

--- src/utils/getPublic.ts
+++ src/utils/getPublic.ts
@@ -5,3 +5,7 @@ export const getPublic = (filePath: string) => BASE_URL + filePath;
 export const getPublicImage = (filePath: string, imagesDirName = 'images') => {
   return getPublic(`${imagesDirName}/${filePath}`);
 };
+
+export const getPublicIcon = (filePath: string, iconsDirName = 'icons') => {
+  return getPublic(`${iconsDirName}/${filePath}`);
+};
