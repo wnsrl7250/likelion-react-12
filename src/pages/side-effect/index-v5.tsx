9565de2bf871c5856015182a4297a6e7a03d576b
wnsrl7250 <wnsrl7250@naver.com>
2025-02-10 18:07:48 +0900

    [실습] 훅 라이프 사이클 다이어그램
    - 클린업 함수 사용
    - 메모리 누수 방지 (성능 저하 문제 원천 차단)

--- src/pages/side-effect/index-v5.tsx
+++ src/pages/side-effect/index-v5.tsx
@@ -1,37 +1,34 @@
 import { useEffect, useState } from 'react';
 
 function SideEffectDemo() {
-  const [count, setCount] = useState(10);
-
+  // [반응성 데이터: 상태] 선언
   const [mouse, setMouse] = useState({ x: 0, y: 0 });
 
+  // [이펙트] 이벤트 구독 / 취소
   useEffect(() => {
-    console.log({ updatedCountValue: count });
+    // 사이드 이펙트 처리
+    // 브라우저 시스템(React의 외부 시스템)과 React 앱 동기화(sync)
+    // 이벤트 구독
+    console.log('이벤트 구독');
 
     const handleMove = (e: PointerEvent) => {
+      // 리액트 상태 업데이트 (리액트 앱과 외부 시스템 동기화)
       setMouse({ x: +e.clientX.toFixed(0), y: +e.clientY.toFixed(0) });
     };
 
-    console.log('이벤트 구독');
     globalThis.addEventListener('pointermove', handleMove);
 
+    // 이벤트 구독 해지
     return () => {
       console.log('이벤트 구독 해지');
       globalThis.removeEventListener('pointermove', handleMove);
     };
-  }, [count]);
+  }, []);
 
   return (
-    <section className="*:text-slate-800 flex flex-col space-y-10">
+    <section className="*:text-slate-800">
       <h2 className="text-2xl font-medium mb-2">React.useEffect 훅 함수</h2>
-      <button
-        type="button"
-        className="px-2 py-1 border text-xl"
-        onClick={() => setCount((c) => c + 2)}
-      >
-        {count}
-      </button>
-      <output className="inline-flex my-5 py-3 px-5 border-6 text-2xl">
+      <output className="inline-flex my-5 py-3 px-5 border-2 text-2xl">
         x = {mouse.x} / y = {mouse.y}
       </output>
     </section>
