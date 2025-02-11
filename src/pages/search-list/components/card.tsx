d58de70b2d3239882fa929bf942524ea0dadc08f
wnsrl7250 <wnsrl7250@naver.com>
2025-02-11 14:32:35 +0900

    [실습] 검색 리스트 4
    - Card 컴포넌트 구성
    - 아이콘 라이브러리 활용
    - 즐겨찾기 버튼 추가

--- src/pages/search-list/components/card.tsx
+++ src/pages/search-list/components/card.tsx
@@ -1,35 +1,59 @@
+import { useState } from 'react';
 import { tm } from '@/utils/tw-merge';
 import { ColorMoodItem } from '../types';
 import generateGradient from '../utils/generate-gradient';
+import { Heart, HeartSolid } from '@mynaui/icons-react';
 
 interface CardProps {
   item: ColorMoodItem;
 }
 
 function Card({ item }: CardProps) {
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  const handleChangeFavorite = () => {
+    const nextIsFavorited = !isFavorited;
+    setIsFavorited(nextIsFavorited);
+  };
+
   const handleLink = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
     console.log(slug);
   };
 
+  const iconSize = 48;
   const slug = `/color-mood/${item.id}`;
+  const buttonLabel = `즐겨찾기 ${isFavorited ? '제거' : '추가'}`;
+  const gradientStyles = { background: generateGradient(item.id) };
+  const Icon = isFavorited ? HeartSolid : Heart;
 
   return (
-    <li className={tm('flex flex-col items-center')}>
+    <li className={tm('flex flex-col items-center gap-3')}>
       <figure
-        role="presentation"
-        className={tm('size-32 rounded-full')}
-        style={{
-          background: generateGradient(item.id),
-        }}
-      ></figure>
+        className={tm('size-32 rounded-full', 'relative')}
+        style={gradientStyles}
+      >
+        <button
+          type="button"
+          title={buttonLabel}
+          aria-label={buttonLabel}
+          onClick={handleChangeFavorite}
+          className={tm(
+            'cursor-pointer',
+            'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2',
+            'rounded-full '
+          )}
+        >
+          <Icon size={iconSize} />
+        </button>
+      </figure>
       <a
         href={slug}
-        className={tm('flex flex-col items-center')}
+        className={tm('flex flex-col items-center gap-0')}
         onClick={handleLink}
       >
-        <h4>{item.title}</h4>
-        <p>{item.description}</p>
+        <h4 className="text-[22px] font-semibold">{item.title}</h4>
+        <p className="text-sm text-slate-700">{item.description}</p>
       </a>
     </li>
   );
--- src/pages/search-list/components/searched-list.tsx
+++ src/pages/search-list/components/searched-list.tsx
@@ -10,7 +10,7 @@ function SearchedList({ list }: SearchedListProps) {
   return (
     <section>
       <h3 className="sr-only">검색된 리스트</h3>
-      <ul className={tm('flex flex-col gap-4')}>
+      <ul className={tm('flex flex-col gap-12')}>
         {list.map((item) => (
           <Card key={item.id} item={item} />
         ))}
