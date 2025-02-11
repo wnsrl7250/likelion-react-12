7bf78ba7143753d20fd61c940d915652d489c778
wnsrl7250 <wnsrl7250@naver.com>
2025-02-11 09:22:08 +0900

    [정리] 관심사 별 묶음
    - 상태 (반응성 데이터)
    - 파생된 상태 (계산된 속성)
    - 이벤트 핸들러 (상태 업데이트 로직 포함)
    - 이펙트 (상태를 종속성으로 관리)

--- src/pages/side-effect/index.tsx
+++ src/pages/side-effect/index.tsx
@@ -1,13 +1,41 @@
 import { useEffect, useState } from 'react';
 
 function SideEffectDemo() {
-  const [count, setCount] = useState(10);
+  // 관심사 별 묶음(상태, 파생된 상태, 상태 업데이트 로직 포함하는 이벤트 핸들러, 종속성 배열에 상태가 포함된 이펙트)
 
+  // [상태]
+  const [count, setCount] = useState(10); // 10
+
+  // [파생된 상태]
+  const doubleCount = count ** 2; // 100
+
+  // [이벤트 핸들러]
+  const handleChangeCount = () => setCount((c) => c + 2);
+
+  // [이펙트]
+  useEffect(() => {
+    console.log({ updatedCountValue: count, doubleCount });
+  }, [count, doubleCount]);
+
+  // --------------------------------------------------------------------------
+
+  // [상태]
   const [mouse, setMouse] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    console.log({ updatedCountValue: count });
+  // [파생된 상태]
+  const mouseOverTheCenterOfTheScreen = mouse.x > globalThis.innerWidth / 2;
 
+  // [이펙트]
+  // mouseOverTheCenterOfTheScreen 파생된 상태가 변경될 때마다 이펙트 함수 실행
+  useEffect(() => {
+    console.log({ mouseOverTheCenterOfTheScreen });
+  }, [mouseOverTheCenterOfTheScreen]);
+
+  // 마운트 시, 1회 이벤트 구독/취소 설정
+  useEffect(() => {
+    // [이벤트 핸들러]
+    // JSX에서 해당 이벤트 핸들러를 사용하는 것이 아니라,
+    // 이펙트 함수 안에서 이벤트 구독, 취소가 이뤄지므로 이 안에 있는 것이 적절하다.
     const handleMove = (e: PointerEvent) => {
       setMouse({ x: +e.clientX.toFixed(0), y: +e.clientY.toFixed(0) });
     };
@@ -19,7 +47,7 @@ function SideEffectDemo() {
       console.log('이벤트 구독 해지');
       globalThis.removeEventListener('pointermove', handleMove);
     };
-  }, [count]);
+  }, []);
 
   return (
     <section className="*:text-slate-800 flex flex-col space-y-10">
@@ -27,7 +55,7 @@ function SideEffectDemo() {
       <button
         type="button"
         className="px-2 py-1 border text-xl"
-        onClick={() => setCount((c) => c + 2)}
+        onClick={handleChangeCount}
       >
         {count}
       </button>
