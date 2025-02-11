6052b2b9ebf548949cc898582ab0fa55ad6e4013
wnsrl7250 <wnsrl7250@naver.com>
2025-02-11 11:44:19 +0900

    [실습] 리스트 렌더링 시 key 속성 값에 index를 사용하면 안되는 이유
    - Lodash의 shuffle 함수 활용
    - 리스트 셔플

--- src/pages/side-effect/index-v7.tsx
+++ src/pages/side-effect/index-v7.tsx
@@ -2,15 +2,18 @@ import { useEffect, useId, useState } from 'react';
 import { tm } from '@/utils/tw-merge';
 import throttle from 'lodash-es/throttle';
 import debounce from 'lodash-es/debounce';
-import shuffle from 'lodash-es/shuffle';
 
 function SideEffectDemo() {
   const throttleTimeId = useId();
-
   const [mouse, setMouse] = useState({ x: 0, y: 0 });
 
+  // [상태]
   const [throttleTime, setThrottleTime] = useState(500);
 
+  // [이벤트 핸들러: 상태 업데이트 로직 포함]
+  // JSX에 연결되는 이벤트 핸들러의 경우, 쓰로틀 또는
+  // 디바운스를 사용해 이벤트 발생 빈도를 조절해야 한다면
+  // value가 아닌, defaultValue 속성을 사용해야 한다.
   const handleChangeThrottleTime = debounce(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       const nextThrottleTime = Number(e.target.value);
@@ -19,6 +22,7 @@ function SideEffectDemo() {
     300
   );
 
+  // [이펙트]
   useEffect(() => {
     const handleMove = throttle((e: PointerEvent) => {
       const x = Number(e.clientX.toFixed(0));
@@ -33,35 +37,6 @@ function SideEffectDemo() {
     };
   }, [throttleTime]);
 
-  // --------------------------------------------------------------------------
-
-  // const [list, setList] = useState<number>([1, 2, 3, 4, 5]);
-  const [list, setList] = useState([
-    {
-      id: 'subject-1',
-      defaultValue: 'react',
-    },
-    {
-      id: 'subject-2',
-      defaultValue: 'react-dom',
-    },
-    {
-      id: 'subject-3',
-      defaultValue: 'react-router',
-    },
-    {
-      id: 'subject-4',
-      defaultValue: 'zustand',
-    },
-  ]);
-
-  console.log(list);
-
-  const handleShuffleList = () => {
-    // 리스트 셔플(섞음)
-    setList(shuffle(list));
-  };
-
   return (
     <section className="flex flex-col items-start">
       <h2 className="text-2xl font-medium">마우스 포인터 움직임 조절</h2>
@@ -75,6 +50,7 @@ function SideEffectDemo() {
             id={throttleTimeId}
             min={10}
             max={1000}
+            // value={throttleTime}
             defaultValue={throttleTime}
             onChange={handleChangeThrottleTime}
           />
@@ -93,39 +69,6 @@ function SideEffectDemo() {
         <span className="font-thin mx-3">/</span> y{' '}
         <span className="font-thin mx-3">=</span> {mouse.y}
       </output>
-
-      <h2 className="mt-6 text-2xl font-medium">
-        key 속성에 index를 사용하면 안되는 이유
-      </h2>
-
-      <button
-        type="button"
-        onClick={handleShuffleList}
-        className={tm(
-          'cursor-pointer',
-          'inline-flex justify-center',
-          'my-5 py-3 px-7 rounded-full',
-          'bg-black text-white text-base font-extrabold',
-          'active:scale-97 active:opacity-80'
-        )}
-      >
-        리스트 셔플
-      </button>
-
-      <ul className="flex flex-col gap-2 *:text-lg *:font-semibold">
-        {list.map((item, index) => (
-          <li key={item.id}>
-            <span className="block mb-2">
-              {index} / [{item.id}]
-            </span>
-            <input
-              type="text"
-              defaultValue={item.defaultValue}
-              className="p-2 bg-white rounded-md"
-            />
-          </li>
-        ))}
-      </ul>
     </section>
   );
 }
