6d4ce96f5331c4bbe20acd3f25105387a02ded90
wnsrl7250 <wnsrl7250@naver.com>
2025-02-11 17:58:28 +0900

    [실습] 검색 리스트 9
    - 공통 로직을 함수로 분리
    - getQueryState

--- src/pages/search-list/components/search-form.tsx
+++ src/pages/search-list/components/search-form.tsx
@@ -1,6 +1,6 @@
 import { useId } from 'react';
 import { tm } from '@/utils/tw-merge';
-import { setQueryParam } from '../utils/query-param';
+import { deleteQueryParam, setQueryParam } from '../utils/query-param';
 
 interface SearchFormProps {
   query: string;
@@ -9,13 +9,18 @@ interface SearchFormProps {
 
 function SearchForm({ query, setQuery }: SearchFormProps) {
   const searchInputId = useId();
+  const nextQuery = query.trim();
 
   const handleQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.currentTarget.value);
   };
 
   const handleSearch = () => {
-    setQueryParam(query);
+    if (nextQuery.length > 0) {
+      setQueryParam(nextQuery);
+    } else {
+      deleteQueryParam();
+    }
   };
 
   return (
--- src/pages/search-list/index.tsx
+++ src/pages/search-list/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { tm } from '@/utils/tw-merge';
 import { getQueryParam } from './utils/query-param';
 import SearchedList from './components/searched-list';
@@ -6,18 +6,39 @@ import SearchForm from './components/search-form';
 import colorMoodList from './data/color-mood-list';
 import { type ColorMoodItem } from './types';
 
+const getQueryState = () => getQueryParam() ?? '';
+
 function SearchListPage() {
   const [list, setList] = useState<ColorMoodItem[]>(colorMoodList);
-
-  // 지연된 초기화(lazy initializer)
-  // useState() 훅에 설정된 함수
-  const [query, setQuery] = useState(() => getQueryParam() ?? '');
 
   const handleUpdateList = (item: ColorMoodItem, isFavorited: boolean) => {
     setList(
       list.map((it) => (it.id === item.id ? { ...it, isFavorited } : it))
     );
   };
+
+  // 지연된 초기화(lazy initializer)
+  // useState() 훅에 설정된 함수
+  const [query, setQuery] = useState(getQueryState);
+
+  // 이펙트 처리
+  useEffect(() => {
+    // popstate 이벤트 구독/해지
+    // 이벤트 핸들러 (동일 참조)
+    const handlePopState = () => {
+      // 브라우저 popstate 이벤트가 감지될 때
+      // 리액트 앱의 query 상태 업데이트 -> UI 화면 업데이트
+      setQuery(getQueryState);
+    };
+
+    // 이벤트 구독
+    globalThis.addEventListener('popstate', handlePopState);
+
+    // 이벤트 해지
+    return () => {
+      globalThis.removeEventListener('popstate', handlePopState);
+    };
+  }, []);
 
   return (
     <section className={tm('flex flex-col gap-5 items-center')}>
