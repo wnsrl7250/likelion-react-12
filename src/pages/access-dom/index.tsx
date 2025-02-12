0fad02b9b138f68455d0449a62b526e5da94f39c
wnsrl7250 <wnsrl7250@naver.com>
2025-02-12 16:42:48 +0900

    [실습] DOM 접근/조작 3
    - VanillaTilt 이펙트 적용
    - 실제 DOM 노드에 이펙트 적용
    - useRef 훅으로 컴포넌트 DOM 참조
    - useEffect 훅을 사용해 참조된 DOM 노드에 VanillaTilt 이펙트 적용

--- src/pages/access-dom/index.tsx
+++ src/pages/access-dom/index.tsx
@@ -1,4 +1,53 @@
 import { useEffect, useRef, useState } from 'react';
+import { Search } from '@mynaui/icons-react';
+import VanillaTilt, { type TiltOptions } from 'vanilla-tilt';
+import { tm } from '@/utils/tw-merge';
+
+const VANILLA_TILT_OPTIONS: TiltOptions = {
+  // 기울이는 방향을 반대로 변경
+  reverse: false,
+  // 최대 기울기 회전(도°)
+  max: 15,
+  // X축의 시작 기울기(도°)
+  startX: 0,
+  // Y축의 시작 기울기(도°)
+  startY: 0,
+  // 관점(perspective) 값이 낮을수록 기울기가 더 커짐
+  perspective: 1000,
+  // 스케일 ( 2 = 200%, 1.5 = 150% )
+  scale: 1.2,
+  // Enter/Exit 전환 속도
+  speed: 600,
+  // Enter/Exit 시 전환 설정
+  transition: true,
+  // "x" 또는 "y" 축 활성화 설정 (`null`일 경우 양쪽 모두 활성화)
+  axis: null,
+  // 종료 시, 기울기 효과를 재설정
+  reset: true,
+  // 종료 재설정이 [0,0](기본값) 또는 [startX, startY]로 이동하는지 여부
+  'reset-to-start': true,
+  // Enter/Exit 시 사용되는 이징 함수
+  easing: 'cubic-bezier(.03,.98,.52,.99)',
+  // "글레어(섬광)" 효과 설정
+  glare: false,
+  // 최대 "글레이" 불투명도 설정 (1 = 100%, 0.5 = 50%)
+  'max-glare': 0.65,
+  // false = VanillaTilt가 글레어 요소를 생성함
+  // true = .js-tilt-glare > .js-tilt-glare-inner를 사용자가 직접 추가해야 함.
+  'glare-prerender': false,
+  // CSS 선택자 또는 HTML 요소 링크 마우스 이벤트 수신
+  'mouse-event-element': undefined,
+  // 장치 방향 감지 활성/비활성화
+  gyroscope: true,
+  // 장치 X축 각도 하한(bottom limit) → 이 각도로 회전된 장치는 마치 마우스가 요소의 왼쪽 테두리에 있는 것처럼 요소를 기울임
+  gyroscopeMinAngleX: -45,
+  // 장치 X축 각도 상한(top limit) → 이 각도로 회전된 장치는 마치 마우스가 요소의 오른쪽 테두리에 있는 것처럼 요소를 기울임
+  gyroscopeMaxAngleX: 45,
+  // 장치 Y축 각도 하한(bottom limit) → 이 각도로 회전된 장치는 마치 마우스가 요소의 위쪽 테두리에 있는 것처럼 요소를 기울임
+  gyroscopeMinAngleY: -45,
+  // 장치 Y축 각도 상한(top limit) → 이 각도로 회전된 장치는 마치 마우스가 요소의 아래쪽 테두리에 있는 것처럼 요소를 기울임
+  gyroscopeMaxAngleY: 45,
+};
 
 function AccessDOMPage() {
   // 컴포넌트 바디(body)
@@ -39,12 +88,38 @@ function AccessDOMPage() {
 
   const searchInputRef = useRef<HTMLInputElement>(null);
 
+  const formRef = useRef<HTMLFormElement>(null);
+
+  useEffect(() => {
+    const formElement = formRef.current;
+    if (formElement) {
+      VanillaTilt.init(formElement, {
+        ...VANILLA_TILT_OPTIONS,
+        glare: true,
+        'max-glare': 1,
+      });
+    }
+  }, []);
+
   useEffect(() => {
     setTimeout(() => {
       if (searchInputRef.current) {
         searchInputRef.current.focus();
       }
     }, 1000);
+  }, []);
+
+  const boxRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const boxElement = boxRef.current;
+    if (boxElement) {
+      VanillaTilt.init(boxElement, {
+        ...VANILLA_TILT_OPTIONS,
+        glare: true,
+        'max-glare': 0.45,
+      });
+    }
   }, []);
 
   // 마크업(markup) 생성
@@ -71,7 +146,7 @@ function AccessDOMPage() {
         DOM 용어 풀이
       </button>
 
-      <form className="my-10">
+      <form ref={formRef} className="my-10 flex">
         <div>
           <label htmlFor="like-a-book" className="sr-only">
             선호 도서
@@ -81,13 +156,23 @@ function AccessDOMPage() {
             id="like-a-book"
             type="search"
             placeholder="좋아하는 도서는?"
-            className="bg-white text-react px-3 py-1.5"
+            className="bg-react text-white px-3 py-2"
           />
         </div>
-        <button type="submit" className="bg-cyan-700 text-white py-2 w-20">
-          저장
+        <button
+          type="submit"
+          aria-label="검색"
+          className={tm(
+            'cursor-pointer',
+            'py-2 w-20 grid place-items-center',
+            'bg-cyan-700 text-white '
+          )}
+        >
+          <Search />
         </button>
       </form>
+
+      <div ref={boxRef} className="size-40 bg-zinc-900" />
     </section>
   );
 }
