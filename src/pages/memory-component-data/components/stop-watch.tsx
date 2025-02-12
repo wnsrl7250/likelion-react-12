c1b42e0c95107ba378f4ade672110e6f62d92c83
wnsrl7250 <wnsrl7250@naver.com>
2025-02-12 15:09:55 +0900

    [실습] StopWatch 컴포넌트 3
    - 타임 포멧팅 해설
    - 버그 개선 (시간 초기화)
    - 정확한 시간 계산 (시작, 진행 시간 동일 / 시작, 일시정지, 정지)

--- src/pages/memory-component-data/components/stop-watch.tsx
+++ src/pages/memory-component-data/components/stop-watch.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { tm } from '@/utils/tw-merge';
 
 /*
@@ -20,23 +20,26 @@ import { tm } from '@/utils/tw-merge';
 const FPS = 1000 / 60;
 
 // 호출 시점의 현재 시간 값 반환 함수
-const getTime = () => Date.now();
+const getDateNow = () => Date.now();
 
 // 타임 포멧팅 함수
 // recordTime + nowTime - startTime
 const formatTime = (time: number) => {
-  // 밀리초(milliseconds)
-  const milliseconds = parseInt(`${time % 100}`, 10);
+  // 밀리초(miliseconds)
+  const miliseconds = parseInt(`${time % 100}`, 10);
+
   // 초(seconds) = 1000ms
   const seconds = parseInt(`${(time / 1000) % 60}`, 10);
-  // 분(minutes) = 60ss
+
+  // 분(minutes) = 60s
   const minutes = parseInt(`${(time / (1000 * 60)) % 60}`, 10);
-  // 시(hours) = 60mm
+
+  // 시(hours) = 60m
   const hours = parseInt(`${(time / (1000 * 60 * 60)) % 60}`, 10);
 
-  // 포멧팅
-  // (반환 값: "hh:mm:ss:ms")
-  const [hh, mm, ss, ms] = [hours, minutes, seconds, milliseconds].map(
+  // 숫자값을 2자리로 설정
+  // 예) 0  →  "00"
+  const [hh, mm, ss, ms] = [hours, minutes, seconds, miliseconds].map(
     (time) => {
       return time.toLocaleString('ko-KR', {
         minimumIntegerDigits: 2,
@@ -47,47 +50,84 @@ const formatTime = (time: number) => {
   return `${hh}:${mm}:${ss}:${ms}`;
 };
 
+// [해설 질문]
+// const timeInfo = formatTime(recordTime + nowTime - startTime);
+//
+// [start]
+// startTime = 21921
+// nowStart = 21921
+// recordTime = 0
+// ... nowStart = 34500
+//
+// [pause] nowStart - startTime = 34500 - 21921 = 12579
+// recordTime = recordTime + nowStart - startTime = 0 + 12579 = <12579>
+//
+// [start]
+// startTime = 31273
+// nowStart = 31273
+// ... nowStart = 61720
+//
+// [pause] nowStart - startTime = 61720 - 31273 = 30447
+// startTime = 81273
+// nowStart = 81273
+// recordTime = recordTime + nowStart - startTime = 12579 + 30447 = <43026>
+//
+// [stop]
+// startTime = 91273
+// nowStart = 91273
+// recordTime = 0
+// recordTime = recordTime + nowStart - startTime = 0 + 0 - 0 = <0>
+
+// setInteval 함수 반환 타입
+type IntervalId = ReturnType<typeof setInterval>;
+
 // 컴포넌트 --------------------------------------------------------
 function StopWatch() {
   // [상태]
-  // 시작 시간
-  const [startTime, setStartTime] = useState(getTime);
+  // 스톱워치 시작 시간
+  const [startTime, setStartTime] = useState(getDateNow);
 
-  // 현재 시간
-  const [nowTime, setNowTime] = useState(getTime);
+  // 스톱워치 시작한 후, 진행되는 현재 시간
+  const [nowTime, setNowTime] = useState(getDateNow);
 
-  // 기록 시간
+  // 스톱워치 일시정지 시, 기록될 시간
   const [recordTime, setRecordTime] = useState(0);
 
-  // 타이머 시작 여부
+  // 스톱워치 시작 여부
   const [isStart, setIsStart] = useState(false);
 
+  // 시작 시간과 진행 시간을 초기화
   const resetTime = () => {
-    setStartTime(getTime);
-    setNowTime(getTime);
+    setStartTime(getDateNow);
+    setNowTime(getDateNow);
   };
+
+  // 렌더링과 무관한 컴포넌트 데이터 기억할 필요성
+  // 브라우저 API: setInterval 중지를 위해
+  // React 탈출구인 useRef 훅을 활용
 
   // [이펙트]
   // 외부 시스템인 브라우저 API와 동기화
-  useEffect(() => {
-    let clearIntervalId: ReturnType<typeof setInterval> | number = 0;
+  // 참조(ref) 객체를 사용해 인터벌을 중지할 ID 기억
+  const clearIntervalIdRef = useRef<IntervalId>(undefined);
 
+  useEffect(() => {
     if (isStart) {
-      // console.log('시작');
       resetTime();
-      clearIntervalId = setInterval(() => {
-        setNowTime(getTime);
+      clearIntervalIdRef.current = setInterval(() => {
+        setNowTime(getDateNow);
       }, FPS);
     } else {
-      // console.log('일시정지');
-      clearInterval(clearIntervalId);
-      // 기록 시간 업데이트
+      clearInterval(clearIntervalIdRef.current);
       setRecordTime((recordTime) => recordTime + nowTime - startTime);
+      resetTime();
     }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isStart]);
 
   // [이벤트 핸들러]
+  // 시작 또는 일시정지 상태 토글 이벤트 핸들러
   const handleStartOrPause = () => {
     setIsStart((s) => !s);
   };
@@ -101,15 +141,15 @@ function StopWatch() {
   // [파생된 상태]
   // 타임 포멧팅
   // "hh:mm:ss:ms"
-  const time = formatTime(recordTime + nowTime - startTime);
+  const timeInfo = formatTime(recordTime + nowTime - startTime);
 
   return (
     <article aria-label="스톱워치" className="flex flex-col gap-2">
       <time
-        dateTime={time}
-        className="px-4 py-2 bg-black text-white text-lg text-center w-46 font-mono"
+        dateTime={timeInfo}
+        className="px-4 py-2 bg-black text-white text-lg text-center w-46 font-mono rounded-full"
       >
-        {time}
+        {timeInfo}
       </time>
       <div className="flex gap-1">
         <button
