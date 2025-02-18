55b89401d8af358d3fdfd39e9a897db2580a580d
wnsrl7250 <wnsrl7250@naver.com>
2025-02-18 13:47:41 +0900

    [실습] 메모리스트 6
    - 메모 삭제 사용자 인터페이스에  피드백 제공
    - 불투명도, 로딩 스피너 표현

--- src/pages/memo-list/components/memo-item.tsx
+++ src/pages/memo-list/components/memo-item.tsx
@@ -2,18 +2,38 @@ import { tm } from '@/utils/tw-merge';
 import { EditOne, TrashOne } from '@mynaui/icons-react';
 import type { MemoItem as MemoItemType } from '../lib/supabase-client';
 import { deleteMemoItem } from '../lib/api';
+import { useState } from 'react';
+import Loading from './loading';
+import delay from '@/utils/delay';
 
 interface MemoItemProps {
   item: MemoItemType;
 }
 
 function MemoItem({ item }: MemoItemProps) {
+  const [isDeleting, setIsDeleting] = useState(false);
+
   const handleDelete = async () => {
+    setIsDeleting(true);
+    await delay(2000);
     await deleteMemoItem(item.id);
+    setIsDeleting(false);
   };
 
   return (
-    <li className="flex flex-col gap-1.5 p-4 bg-react text-white rounded-sm">
+    <li
+      className={tm(
+        'flex flex-col gap-1.5 p-4 bg-react text-white rounded-sm',
+        { 'relative bg-react/70': isDeleting }
+      )}
+    >
+      {isDeleting && (
+        <Loading
+          label="삭제 중..."
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-10"
+        />
+      )}
+
       <h3 className="font-light tracking-wide text-xl text-sky-500">
         {item.title}
       </h3>
