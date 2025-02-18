09b55f8050a6cc1bc5709b35389a3031a5009b7d
wnsrl7250 <wnsrl7250@naver.com>
2025-02-18 14:35:31 +0900

    [실습] 메모리스트 7 - 수정, 저장 모드 상태 선언 - 수정 기능 구현 - 메모 수정 사용자 인터페이스에  피드백 제공 - 불투명도, 로딩 스피너 표현

--- src/pages/memo-list/components/memo-item.tsx
+++ src/pages/memo-list/components/memo-item.tsx
@@ -1,8 +1,11 @@
 import { tm } from '@/utils/tw-merge';
-import { EditOne, TrashOne } from '@mynaui/icons-react';
-import type { MemoItem as MemoItemType } from '../lib/supabase-client';
-import { deleteMemoItem } from '../lib/api';
-import { useState } from 'react';
+import { EditOne, EditOneSolid, TrashOne } from '@mynaui/icons-react';
+import type {
+  MemoItem as MemoItemType,
+  MemoItemUpdate,
+} from '../lib/supabase-client';
+import { deleteMemoItem, editMemoItem } from '../lib/api';
+import { useEffect, useRef, useState } from 'react';
 import Loading from './loading';
 import delay from '@/utils/delay';
 
@@ -11,6 +14,7 @@ interface MemoItemProps {
 }
 
 function MemoItem({ item }: MemoItemProps) {
+  // [관심사] 삭제 상태와 업데이트 핸들러
   const [isDeleting, setIsDeleting] = useState(false);
 
   const handleDelete = async () => {
@@ -20,36 +24,87 @@ function MemoItem({ item }: MemoItemProps) {
     setIsDeleting(false);
   };
 
+  // [관심사] 수정 상태와 업데이트 핸들러
+  const [isEditing, setIsEditing] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
+
+  useEffect(() => {
+    if (isEditing) {
+      titleRef.current?.focus();
+    }
+  }, [isEditing]);
+
+  const editButtonLabel = isEditing ? '저장' : '수정';
+
+  const handleChangeEditMode = () => {
+    setIsEditing(true);
+  };
+
+  const handleSaveMemo = async () => {
+    const titleElement = titleRef.current!;
+    const contentElement = contentRef.current!;
+    const currentTime = new Date().toISOString();
+
+    const willEditMemoItem = {
+      ...item,
+      title: titleElement.textContent,
+      content: contentElement.textContent,
+      updated_at: currentTime,
+    } as MemoItemUpdate;
+
+    setIsSaving(true);
+    await delay(900);
+    await editMemoItem(willEditMemoItem);
+    // 서버에 저장한 후 모드 변경
+    setIsSaving(false);
+    setIsEditing(false);
+  };
+
+  const titleRef = useRef<HTMLHeadingElement>(null);
+  const contentRef = useRef<HTMLParagraphElement>(null);
+
   return (
     <li
       className={tm(
         'flex flex-col gap-1.5 p-4 bg-react text-white rounded-sm',
-        { 'relative bg-react/70': isDeleting }
+        { 'relative bg-react/70': isDeleting || isSaving }
       )}
     >
-      {isDeleting && (
+      {(isDeleting || isSaving) && (
         <Loading
-          label="삭제 중..."
+          label={isSaving ? '저장 중...' : '삭제 중...'}
           className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-10"
         />
       )}
 
-      <h3 className="font-light tracking-wide text-xl text-sky-500">
+      <h3
+        ref={titleRef}
+        contentEditable={isEditing}
+        suppressContentEditableWarning
+        className="font-light tracking-wide text-xl text-sky-500"
+      >
         {item.title}
       </h3>
-      <p className="text-sm text-slate-400 leading-relaxed">{item.content}</p>
+      <p
+        ref={contentRef}
+        contentEditable={isEditing}
+        suppressContentEditableWarning
+        className="text-sm text-slate-400 leading-relaxed"
+      >
+        {item.content}
+      </p>
       <div role="group" className="flex gap-1">
         <button
           type="button"
-          aria-label="수정"
-          title="수정"
+          aria-label={editButtonLabel}
+          title={editButtonLabel}
+          onClick={!isEditing ? handleChangeEditMode : handleSaveMemo}
           className={tm(
             'cursor-pointer',
             'size-5 opacity-75 hover:opacity-100'
           )}
         >
-          <EditOne size={20} />
-          {/* <EditOneSolid size={20} /> */}
+          {!isEditing ? <EditOne size={20} /> : <EditOneSolid size={20} />}
         </button>
         <button
           type="button"
