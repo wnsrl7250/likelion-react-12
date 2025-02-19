35b3090942acad4271e4849a0a71cffc827ce2a4
wnsrl7250 <wnsrl7250@naver.com>
2025-02-19 10:36:00 +0900

    [실습] 쿼리 옵션을 컴포넌트 외부로 분리 - 서버의 비동기 상태를 효과적으로 관리, 화면 업데이트

--- src/pages/custom-hook/components/pokemon-with-use-query.tsx
+++ src/pages/custom-hook/components/pokemon-with-use-query.tsx
@@ -3,43 +3,70 @@ import Loading from '@/pages/memo-list/components/loading';
 import type { Pokemon } from '../types';
 import useQuery from '@/hooks/use-query';
 
+const pikachuQueryOptions = {
+  queryKey: '@pokemon/pikachu',
+  queryFn: () => fetch('https://pokeapi.co/api/v2/pokemon/25'),
+};
+
+const raichuQueryOptions = {
+  queryKey: '@pokemon/raichu',
+  queryFn: () => fetch('https://pokeapi.co/api/v2/pokemon/26'),
+};
+
 function PokemonWithUseQuery() {
   // 포켓몬 개별 정보 가져오기
   // 'https://pokeapi.co/api/v2/pokemon/1'
 
-  const {
-    isLoading: loading,
-    error,
-    data,
-  } = useQuery<Pokemon>({
-    queryKey: '@pokemon/pikachu',
-    queryFn: () => fetch('https://pokeapi.co/api/v2/pokemon/25'),
-  });
-
-  console.log(data);
+  const pihachu = useQuery<Pokemon>(pikachuQueryOptions);
+  const raichu = useQuery<Pokemon>(raichuQueryOptions);
 
   return (
-    <figure
-      className={tm(
-        'flex gap-5 justify-center items-center',
-        'border-4 border-black/10',
-        'rounded-full h-42',
-        'transition-colors duration-250 ease-in-out',
-        'hover:border-black'
-      )}
-    >
-      {/* <Loading size={24} label="데이터 로딩 중..." className="text-black/50" /> */}
-      {loading && <Loading size={48} label="포켓몬 데이터 로딩 중..." />}
-      {error && <div role="alert">{error.message}</div>}
-      {!loading && data && (
-        <img
-          src={data.sprites.front_default}
-          alt={data.name}
-          title={data.name}
-          className="size-40"
-        />
-      )}
-    </figure>
+    <>
+      <figure
+        className={tm(
+          'flex gap-5 justify-center items-center',
+          'border-4 border-black/10',
+          'rounded-full h-42',
+          'transition-colors duration-250 ease-in-out',
+          'hover:border-black'
+        )}
+      >
+        {pihachu.isLoading && (
+          <Loading size={48} label="포켓몬 데이터 로딩 중..." />
+        )}
+        {pihachu.error && <div role="alert">{pihachu.error.message}</div>}
+        {!pihachu.isLoading && pihachu.data && (
+          <img
+            src={pihachu.data.sprites.front_default}
+            alt={pihachu.data.name}
+            title={pihachu.data.name}
+            className="size-40"
+          />
+        )}
+      </figure>
+      <figure
+        className={tm(
+          'flex gap-5 justify-center items-center',
+          'border-4 border-black/10',
+          'rounded-full h-42',
+          'transition-colors duration-250 ease-in-out',
+          'hover:border-black'
+        )}
+      >
+        {raichu.isLoading && (
+          <Loading size={48} label="포켓몬 데이터 로딩 중..." />
+        )}
+        {raichu.error && <div role="alert">{raichu.error.message}</div>}
+        {!raichu.isLoading && raichu.data && (
+          <img
+            src={raichu.data.sprites.front_default}
+            alt={raichu.data.name}
+            title={raichu.data.name}
+            className="size-40"
+          />
+        )}
+      </figure>
+    </>
   );
 }
 
