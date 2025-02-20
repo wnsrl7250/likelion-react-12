614f0a8a04cdc438468f8758b39020828482ba47
wnsrl7250 <wnsrl7250@naver.com>
2025-02-20 18:02:49 +0900

    [실습] Zustand 스토어 및 훅 함수 생성 2 - 상태 타입 정의 및 선언 - 액션 타입 정의 및 선언 - 스토어 생성 후, 훅 함수 생성

---
+++ src/stores/counts.ts
@@ -0,0 +1,38 @@
+import { create } from 'zustand';
+
+interface State {
+  count: number;
+  step: number;
+  min: number;
+  max: number;
+}
+
+interface Actions {
+  count: number;
+  step: number;
+  min: number;
+  max: number;
+  increment: () => void;
+  decrement: () => void;
+  update: (value: number) => void;
+  reset: () => void;
+  setStep: (value: number) => void;
+  setMin: (value: number) => void;
+  setMax: (value: number) => void;
+}
+
+type Store = State & Actions;
+
+export const useCountStore = create<Store>((set) => ({
+  count: 0,
+  step: 1,
+  min: 1,
+  max: 10,
+  increment: () => set((state) => ({ count: state.count + 1 })),
+  decrement: () => set((state) => ({ count: state.count - 1 })),
+  update: (value) => set({ count: value }),
+  setStep: (value) => set({ step: value }),
+  setMin: (value) => set({ min: value }),
+  setMax: (value) => set({ max: value }),
+  reset: () => set({ count: 0 }),
+}));
